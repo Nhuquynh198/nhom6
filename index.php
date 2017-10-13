7411f1dcdad93bcbe74a0d8b57c7d66796e8a57e
0979848986 <dmt6688@gmail.com>
2017-10-13 16:47:12 +0700

    dtt

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	function So_chinh_phuong()
+	{
+		$x;
+		$ket_qua = sqrt($x);
+		if($x > 0)
+		{		
+			if($ket_qua == int($ketqua))
+			{
+				echo "la so chinh phuong";
+			}
+			else
+			{
+				echo "khong la so chinh phuong";	
+			}
+		}
+		else
+			echo "So ban vua nhap khong hop le";
+	}
+?>
---
+++ so_chinh_phuong.php
@@ -0,0 +1,20 @@
+<?php
+	function so_chinh_phuong()
+	{
+		$x;
+		if(x>0)
+		{
+			$ket_qua = sqrt($x);
+			if($ket_qua == int($ket_qua))
+			{
+				echo "la so chinh phuong";	
+			}
+			else
+				echo "khong la so chinh phuong";	
+		}	
+		else
+		{
+				echo "so ban vua nhap khong hop le";
+		}
+	}
+?>
